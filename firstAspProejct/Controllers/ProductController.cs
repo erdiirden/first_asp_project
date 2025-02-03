da8ed774304157042e25e725f4badc90ec57af35
Erdi İrden <113932351+erdiirden@users.noreply.github.com>
2025-02-03 16:54:09 +0300

    cvm
    controllers, models, views...

---
+++ firstAspProejct/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using firstAspProejct.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstAspProejct.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts() //view dosyası da aynı isimde olmalı (GetProducts)
+        {
+            //--ŞUAN CONTROLLER DAYIZ--
+
+            //--VERİ ALINACAKSA MODELE GİDİYORUZ VE ALIYORUZ--
+            Products products = new Products(); //model a gidip veri alma
+
+            //--TASARIM ALINACAKSA VİEW A GİDİYORUZ VE ALIYORUZ--
+            return View(); //response (default olarak aynı isimdeki view i çağırır(getproducts)
+
+
+            // return View("ali"); //manuel olarak (ali) view çağırma
+        }
+        
+    }
+}
---
+++ firstAspProejct/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace firstAspProejct.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
