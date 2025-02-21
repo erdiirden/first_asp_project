f3ad2641c2796802f5ab710872bcc29f53c695b6
Erdi İrden <113932351+erdiirden@users.noreply.github.com>
2025-02-21 06:48:31 +0300

    ModelMetadataType
    -ModelMetadataType İle Validation Sorumluluğunu Başka Bir Sınıfa Yükleme
    
    -Validationun düzenli kullanımı (yeni klasör ve dosyada)
    
    -2. dosya

---
+++ 2/deneme/Models/ModelMetaDataTypes/ProductMetaData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace deneme.Models.ModelMetaDataTypes
+{
+    public class ProductMetaData
+    {
+        [Required(ErrorMessage = "lütfen product name gir")]
+        [StringLength(100, ErrorMessage = "max 100 karakter giriniz")]
+        public string ProductName { get; set; }
+
+        [EmailAddress(ErrorMessage = "email formatında gir")]
+        public string Email { get; set; }
+    }
+}
--- 2/deneme/Models/Product.cs
+++ 2/deneme/Models/Product.cs
@@ -1,14 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using deneme.Models.ModelMetaDataTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace deneme.Models
 {
+    [ModelMetadataType(typeof(ProductMetaData))]
     public class Product
     {
-        [Required(ErrorMessage ="lütfen product name gir")]
-        [StringLength(100,ErrorMessage ="max 100 karakter giriniz")]
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        [EmailAddress(ErrorMessage ="email formatında gir")]
         public string Email { get; set; }
     }
 }
